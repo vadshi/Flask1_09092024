b5b4604e4687542f211b641775d1e73e8fc7b5a2
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-09-16 19:40:00 +0300

    Done CRUD with SQLAlchemy

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from werkzeug.exceptions import HTTPException
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
-
+from sqlalchemy import String, func
+from sqlalchemy.exc import InvalidRequestError
 
 class Base(DeclarativeBase):
     pass
@@ -69,107 +69,92 @@ def get_quotes() -> list[dict[str, Any]]:
 @app.route("/quotes/<int:quote_id>")
 def get_quote(quote_id: int) -> dict:
     """ Функция возвращает цитату по значению ключа id=quote_id."""
-    select_quote = "SELECT * FROM quotes WHERE id = ?"
-    cursor = get_db().cursor()
-    cursor.execute(select_quote, (quote_id,))
-    quote_db = cursor.fetchone()  # Получаем одну запись из БД
-    if quote_db:
-        keys = ("id", "author", "text", "rating")
-        quote = dict(zip(keys, quote_db))
-        return jsonify(quote), 200 
-    return {"error": f"Quote with id={quote_id} not found"}, 404               
+    quote = db.get_or_404(quote_id)
+    return jsonify(quote.to_dict()), HTTPStatus.OK
 
 
 @app.get("/quotes/count")
 def quotes_count():
-    """Function for task3 of Practice part1."""
-    select_count = "SELECT count(*) as count FROM quotes"
-    cursor = get_db().cursor()
-    cursor.execute(select_count)
-    count = cursor.fetchone()
-    if count:
-        return jsonify(count=count[0]), 200
-    abort(503)  # вернем ошибку 503
+    """Function to count all quotes."""
+    count = db.session.scalar(func.count(QuoteModel.id))
+    return jsonify(count=count), 200
+
 
 
 @app.route("/quotes", methods=['POST'])
 def create_quote():
-    new_quote = request.json
-    insert_quote = "INSERT INTO quotes (author, text, rating) VALUES (?, ?, ?)"
-    connection = get_db()
-    cursor = connection.cursor()
-    cursor.execute(insert_quote, (new_quote['author'], new_quote['text'], new_quote['rating']))
-    answer = cursor.lastrowid  # Получаем из БД id новой цитаты
-    connection.commit()
-    new_quote['id'] = answer
-    return jsonify(new_quote), 201
+    data = request.json
+
+    if "rating" not in data or not data["rating"] in range(1, 6):
+        data["rating"] = 1
+
+    try:
+        quote = QuoteModel(**data)
+        db.session.add(quote)
+        db.session.commit()
+    except Exception as e:
+        abort(503, f"error: {e.description}")
+    except TypeError:
+        return (
+            (
+                "Invalid data. Required: author, text, rating (optional). "
+                f"Received: {", ".join(data.keys())}"
+            ),
+            HTTPStatus.BAD_REQUEST,
+        )
+
+    return quote.to_dict(), HTTPStatus.CREATED
 
 
 @app.route("/quotes/<int:quote_id>", methods=["PUT"])
 def edit_quote(quote_id):
-    new_data = request.json
-    attributes: set = set(new_data.keys()) & {'author', 'rating', 'text'}
+    quote: QuoteModel = db.get_or_404(quote_id)
 
-    if "rating" in attributes and new_data["rating"] not in range(1, 6):
-        # Валидируем новое значени рейтинга и случае успеха обновляем данные
-        attributes.remove("rating")
-    if attributes:
-        update_quotes = f"UPDATE quotes SET {', '.join(attr + '=?' for attr in attributes)} WHERE id=?"
-        params = tuple(new_data.get(attr) for attr in attributes) + (quote_id,)
-        connection = get_db()
-        cursor = connection.cursor()
-        cursor.execute(update_quotes, params)
-        rows = cursor.rowcount  
-        if rows:
-            connection.commit()
-            cursor.close()
-            responce, status_code = get_quote(quote_id)
-            if status_code == 200:
-                return responce, HTTPStatus.OK
-        connection.rollback()
-    else:
-        responce, status_code = get_quote(quote_id)
-        if status_code == 200:
-            return responce, HTTPStatus.OK
-    abort(404, f"Quote with id={quote_id} not found.")
+    data: dict = request.json
+    if "rating" in data and not data["rating"] in range(1, 6):
+        data.pop("rating")
+    if len(data) == 0:
+        return "No valid data to update", HTTPStatus.BAD_REQUEST
+
+    try:
+        for key, value in data.items():
+            if not hasattr(quote, key):
+                raise Exception(f"Invalid key: {key}. Valid: author, text, rating")
+            setattr(quote, key, value)
+        db.session.commit()
+        return quote.to_dict()
+    except Exception as e:
+        return str(e), HTTPStatus.BAD_REQUEST
 
 
 @app.route("/quotes/<int:quote_id>", methods=["DELETE"])
 def delete_quote(quote_id: int):
-    delete_sql = f"DELETE FROM quotes WHERE id = ?"
-    params = (quote_id,)
-    connection = get_db()
-    cursor = connection.cursor()
-    cursor.execute(delete_sql, params)  
-    rows = cursor.rowcount  # Кол-во измененных строк
-    if rows:
-        connection.commit()
-        cursor.close()         
-        return jsonify({"message": f"Quote with id {quote_id} has deleted."}), 200
-    connection.rollback()
-    abort(404, f"Quote with id={quote_id} not found")
+    quote = db.get_or_404(quote_id)
+    db.session.delete(quote)
+    try:
+        db.session.commit()
+        return f"Quote with id {id} deleted"
+    except Exception as e:
+        db.session.rollback()
+        abort(503, f"Database error: {e.description}")
 
 
-# @app.route("/quotes/filter")
-# def filter_quotes():
-#    """TODO: change to work with database."
-#    filtered_quotes = quotes.copy()
-#    # request.args хранит данные, полученные из query parameters
-#    for key, value in request.args.items():
-#       if key not in ("author", "rating"):
-#          return f"Invalid key {key}", HTTPStatus.BAD_REQUEST
-#       if key == "rating":
-#          value = int(value)
-#       filtered_quotes = [quote for quote in filtered_quotes if quote.get(key) == value]
-#       # ======== the same as 136 ==========
-#       # res_quotes = []
-#       # for quote in filtered_quotes:
-#       #    if quote[key] == value:
-#       #       res_quotes.append(quote)
-#       # filtered_quotes = res_quotes.copy() # Делаю независимую копию списка
-#       # ===================================
-#    return filtered_quotes
+@app.route("/quotes/filter")
+def filter_quotes():
+    """DONE: change to work with database."""
+    try:
+        quotes = db.session.scalars(QuoteModel).filter_by(**request.args).all()
+    except InvalidRequestError:
+        return (
+            (
+                "Invalid data. Possible keys: author, text, rating. "
+                f"Received: {", ".join(request.args.keys())}"
+            ),
+            HTTPStatus.BAD_REQUEST,
+        )
+
+    return jsonify([quote.to_dict() for quote in quotes]), 200
 
 
 if __name__ == "__main__":
-   app.run(debug=True)
+   app.run(debug=True, port=5000)
